84ae981e71e318570ed084ebdb2c136fb1c90931
mustafa oğuzhan ün <mustafaoguzhanun@mustafa-MacBook-Air.local>
2019-02-12 16:19:22 +0300

    socket.broadcast.emit and io.emit methods added

--- server/server.js
+++ server/server.js
@@ -19,24 +19,52 @@ var io = socketIO(server);
 io.on('connection',(socket)=>{
     
     console.log('client has connected...');
+
+    socket.emit("newMessage",{
+        text : "Welcome to the chat app...",
+        createdAt : new Date().getTime(),
+        from : "Admin"
+    });
+
+    socket.broadcast.emit("newMessage", {
+        from : "Admin",
+        text : "A new friend joined us"
+    })
+
+    // birinden bir mesaj gelirse diye dinliyor...
+    socket.on("createMessage", (message)=>{
+        console.log("message :", message);
+
+        // mesaj geldiğinde bunu herkese, gönderen dahil gönderiyor...
+        // io.emit("newMessage", {
+        //     text : message.text,
+        //     from : message.from,
+        //     createdAt : new Date().getTime()
+        // })
+
+        // burada ise gönderiyi yapan dışında geri kalan herkese mesaj iletiliyor...
+        // socket.broadcast.emit("newMessage",{
+        //     text : message.text,
+        //     from : message.from,
+        //     createdAt : new Date().getTime()
+        // })
+    })
+
+    // socket.on("createEmail", (email)=>{
+    //     console.log("new email:", email);
+    // })
+
+    //yalnız bir adrese göndermek için kullanılıyor...
+    // socket.emit("newEmail",{ from:"jessica@simpson.com",text:"hey there i am using whatsapp",createdAt:123 } );
+
+    // yalnız bir adrese gönderi yapıyor...
+    // socket.emit("newMessage", {from:"mr.bean",text:"hello",createdAt:123});
     
     // disconnect işlemi io.on içinde aranıyor...
     socket.on("disconnect",()=>{
         console.log("client disconnected...")
     })
 
-    socket.on("createEmail", (email)=>{
-        console.log("new email:", email);
-    })
-
-    socket.emit("newEmail",{ from:"jessica@simpson.com",text:"hey there i am using whatsapp",createdAt:123 } );
-
-    socket.emit("newMessage", {from:"mr.bean",text:"hello",createdAt:123});
-    
-    socket.on("createMessage", (message)=>{
-        console.log("message :", message);
-    })
-    
 })
 
 server.listen(port, ()=>{
