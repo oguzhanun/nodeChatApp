9ffbe4ba5826abc0c78807cdbe13ba4dfc496493
mustafa oğuzhan ün <mustafaoguzhanun@mustafa-MacBook-Air.local>
2019-02-12 13:10:22 +0300

    connection with socket.io

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 var staticFolder = path.join(__dirname,"../public");
 
@@ -9,11 +11,27 @@ app.use(express.static(staticFolder));
 
 var port = process.env.PORT || 3000;
 
-app.get('/',(req,res)=>{
-    res.render("index.html")
+var server = http.createServer(app);
+
+// server socket i elde edilmiş oluyor bu şekilde...
+var io = socketIO(server);
+
+io.on('connection',(socket)=>{
+    
+    console.log('client has connected...');
+    
+    // disconnect işlemi io.on içinde aranıyor...
+    socket.on("disconnect",()=>{
+        console.log("client disconnected...")
+    })
+
 })
 
-app.listen(port, ()=>{
+// app.get('/',(req,res)=>{
+//     res.render("index.html")
+// })
+
+server.listen(port, ()=>{
     console.log("server is up and running on port " + port);
 })
 
